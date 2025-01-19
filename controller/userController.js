a4a20ec1b8497e4099d9e46b58070f1bc047fcbe
Cecilia Mirea <mireamaria22@stud.ase.ro>
2025-01-19 22:47:50 +0200

    fixed sth in userController

--- controller/userController.js
+++ controller/userController.js
@@ -16,8 +16,13 @@ const loginUser=async(req, res)=>{
     try{
         const body=req.body;
         const user=await User.findOne({userName:body.userName, userPassword:body.userPassword});
+        const pass=await User.findOne({userPassword:body.userPassword});
+        const uname=await User.findOne({userName:body.userName});
         if(user){
             res.status(200).json(user);
+        }
+        else if(!pass && uname || pass && !uname){
+            res.status(401).json(pass);
         }
         else{
             res.status(404).json({msg:"User not found"});
